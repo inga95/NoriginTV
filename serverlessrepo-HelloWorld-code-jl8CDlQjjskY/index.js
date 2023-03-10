88cc3c65b408c962de1fd19fad94d735a5f984a7
Inga <ingabr1@hotmail.com>
2023-03-10 14:28:15 +0100

    made a random function and movie by genre function

--- serverlessrepo-HelloWorld-code-jl8CDlQjjskY/index.js
+++ serverlessrepo-HelloWorld-code-jl8CDlQjjskY/index.js
@@ -1,20 +1,193 @@
-// Include the Alexa SDK v2
+// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+// session persistence, api calls, and more.
 const Alexa = require("ask-sdk-core");
+const fs = require("fs");
 
-// The "LaunchRequest" intent handler - called when the skill is launched
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === "LaunchRequest";
   },
   handle(handlerInput) {
-    const speechText = "Halla Verden!!";
-
-    // Speak out the speechText via Alexa
-    return handlerInput.responseBuilder.speak(speechText).getResponse();
+    const speechText =
+      "Welcome to Norigin TV's Alexa helper. What can I do for you?";
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
   },
 };
 
-// Register the handlers and make them ready for use in Lambda
+// Reads the contents of the movies.json file
+const movies = JSON.parse(fs.readFileSync("./movies.json"));
+
+function getRandomMovie(movies) {
+  const index = Math.floor(Math.random() * movies.length);
+  return movies[index];
+}
+
+const MovieRecommendationIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name ===
+        "MovieRecommendationIntent"
+    );
+  },
+  handle(handlerInput) {
+    const movie = getRandomMovie(movies);
+    const speechText = `I recommend watching ${movie.title}. It is a ${movie.genre} movie and it has a score of ${movie.imdb} on IMDB. The film is about ${movie.description}`;
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const MovieByGenreIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "MovieByGenreIntent"
+    );
+  },
+  handle(handlerInput) {
+    const genre =
+      handlerInput.requestEnvelope.request.intent.slots.genre.value.toLowerCase();
+
+    const genreMovies = movies.filter((movie) => movie.genre.includes(genre));
+
+    if (genreMovies.length === 0) {
+      return handlerInput.responseBuilder
+        .speak(`Sorry, I could not find any movies in the ${genre} genre.`)
+        .getResponse();
+    }
+
+    const movieIndex = Math.floor(Math.random() * genreMovies.length);
+    const movie = genreMovies[movieIndex];
+
+    const speechText = `I recommend watching ${movie.title}. It is a very good ${movie.genre} movie!`;
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const HelloWorldIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "HelloWorldIntent"
+    );
+  },
+  handle(handlerInput) {
+    const speechText = "Hello, I'm Alexa. How are you?";
+    return (
+      handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+        .getResponse()
+    );
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "AMAZON.HelpIntent"
+    );
+  },
+  handle(handlerInput) {
+    const speechText = "You can say hello to me! How can I help?";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      (handlerInput.requestEnvelope.request.intent.name ===
+        "AMAZON.CancelIntent" ||
+        handlerInput.requestEnvelope.request.intent.name ===
+          "AMAZON.StopIntent")
+    );
+  },
+  handle(handlerInput) {
+    const speechText = "Goodbye!";
+    return handlerInput.responseBuilder.speak(speechText).getResponse();
+  },
+};
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === "SessionEndedRequest";
+  },
+  handle(handlerInput) {
+    // Any cleanup logic goes here.
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === "IntentRequest";
+  },
+  handle(handlerInput) {
+    const intentName = handlerInput.requestEnvelope.request.intent.name;
+    const speechText = `You just triggered ${intentName}`;
+
+    return (
+      handlerInput.responseBuilder
+        .speak(speechText)
+        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+        .getResponse()
+    );
+  },
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`~~~~ Error handled: ${error.message}`);
+    const speechText = `Sorry, I couldn't understand what you said. Please try again.`;
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+// This handler acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
-  .addRequestHandlers(LaunchRequestHandler)
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    HelloWorldIntentHandler,
+    MovieRecommendationIntentHandler,
+    MovieByGenreIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+    IntentReflectorHandler
+  ) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+  .addErrorHandlers(ErrorHandler)
   .lambda();
